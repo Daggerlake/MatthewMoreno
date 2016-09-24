5f49c72476837e83ee0733558173c83df6c82792
Matthew Moreno <mmore500@gmail.com>
2016-09-24 10:06:25 -0700

    Add graceful Mongoose connection shutdowns

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -11,3 +11,38 @@ mongoose.connection.on('error', function (err) {
 mongoose.connection.on('disconnected', function () {
   console.log('Mongoose disconnected');
 });
+
+// close Mongoose connection, passing through an anonymous function to run
+// when closed
+// arguments: message and callback function
+var gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+// listen for SIGUSR2, which is what nodemon uses
+process.once('SIGUSR2', function () {
+  // send message to gracefulShutdown and callback to
+  // kill process, emitting SIGUSR2 again
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+// listen for SIGINT emitted on application termination
+process.on('SIGINT', function () {
+  // send message to gracefulShutdown and callback to exit Node process
+  gracefulShotdown('app termination', function () {
+    process.exit(0);
+  });
+});
+
+// listen for SIGTERM emitted when Heroku shuts down process
+process.on('SIGTERM', function () {
+  // send message to gracefulShutdown and callback to exit Node process
+  gracefulShotdown('Heroku app shutdown', function () {
+    process.exit(0);
+  });
+});
