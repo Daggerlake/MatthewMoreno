6cc78fbdef1a030fdadd69a674f14fc307a8f13a
Matthew Moreno <mmore500@gmail.com>
2016-09-25 11:14:39 -0700

    Add sendJsonResponse to placeholder controllers

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,4 +1,16 @@
-module.exports.reviewsCreate = function(req, res) { };
-module.exports.reviewsReadOne = function(req, res) { };
-module.exports.reviewsUpdateOne = function(req, res) { };
-module.exports.reviewsDeleteOne = function(req, res) { };
+var sendJsonResponse = function(res, status, content) {  res.status(status);
+  res.json(content);
+};
+
+module.exports.reviewsCreate = function(req, res) {
+  sendJsonResponse(res, 200, {"status" : "success"});
+};
+module.exports.reviewsReadOne = function(req, res) {
+  sendJsonResponse(res, 200, {"status" : "success"});
+};
+module.exports.reviewsUpdateOne = function(req, res) {
+  sendJsonResponse(res, 200, {"status" : "success"});
+};
+module.exports.reviewsDeleteOne = function(req, res) {
+  sendJsonResponse(res, 200, {"status" : "success"});
+};
