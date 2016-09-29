03eb11c2b78b0b94b12bbb734ae09ebbb724ad5d
Matthew Moreno <mmore500@gmail.com>
2016-09-29 16:46:50 -0700

    Implement locationsUpdateOne controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -120,7 +120,54 @@ module.exports.locationsReadOne = function(req, res) {
   }
 };
 module.exports.locationsUpdateOne = function(req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
+  if (!req.params.locationid) {
+    sendJsonResponse(res, 404, {
+      "message": "Not found, locationid is required"
+    });
+    console.log("locationsUpdateOne locationid is required");
+    return;
+  }
+  // find location document by supplied id
+  Loc
+    .findById(req.params.locationid)
+    .select('-reviews -rating')
+    .exec(
+      function(err, location) {
+        if (!location) {
+          sendJsonResponse(res, 404, {
+            "message": "locationid not found"
+          });
+          console.log("locationsUpdateOne locationid not found");
+          return;
+        } else if (err) {
+          sendJsonResponse(res, 400, err);
+          return;
+        }
+        // update paths with values from submitted form
+        location.name = req.body.name;
+        location.address = req.body.address;
+        location.facilities = req.body.facilities.split(",");
+        location.coords = [parseFloat(req.body.lng), parseFloat(req.body.lat)];
+        location.openingTimes = [{
+          days: req.body.days1,
+          opening: req.body.opening1,
+          closed: req.body.closed1,
+        }, {
+          days: req.body.days2,
+          opening: req.body.opening2,
+          closing: req.body.closing2,
+          closed: req.body.closed2,
+        }];
+        // save instance, sending approprate response depending on outcome
+        location.save(function(err, location) {
+          if (err) {
+            sendJsonResponse(res, 404, err);
+          } else {
+            sendJsonResponse(res, 200, location);
+          }
+        });
+      }
+    );
 };
 module.exports.locationsDeleteOne = function(req, res) {
   sendJsonResponse(res, 200, {"status" : "success"});
