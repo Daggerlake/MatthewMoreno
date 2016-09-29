5944966aefccd9d3fb36605202cf29c23770ebd1
Matthew Moreno <mmore500@gmail.com>
2016-09-29 15:34:12 -0700

    Implement locationsCreate controller

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -58,7 +58,35 @@ module.exports.locationsListByDistance = function(req, res) {
 };
 
 module.exports.locationsCreate = function(req, res) {
-  sendJsonResponse(res, 200, {"status" : "success"});
+  Loc.create({
+    name: req.body.name,
+    address: req.body.address,
+    // create array of facilities by splitting a
+    // comma-separated list
+    facilities: req.body.facilities.split(","),
+    // parse coordinates from strings to numbers
+    coords: [parseFloat(req.body.lng), parseFloat(req.body.lat)],
+    openingTimes: [{
+      days: req.body.days1,
+      opening: req.body.opening1,
+      closing: req.body.closing1,
+      closed: req.body.closed1,
+    }, {
+      days: req.body.days2,
+      opening: req.body.opening2,
+      closing: req.body.closing2,
+      closed: req.body.closed2,
+    }]
+  },
+  // suppply callback funciton, containing appropriate
+  // responses for success and failure
+  function (err, location) {
+    if (err) {
+      sendJsonResponse(res, 400, err);
+    } else {
+      sendJsonResponse(res, 201, location);
+    }
+  });
 };
 
 module.exports.locationsReadOne = function(req, res) {
