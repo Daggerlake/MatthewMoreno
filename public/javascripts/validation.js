09e7c1c3778858934fa4001b00f3beb23855c4aa
Matthew Moreno <mmore500@gmail.com>
2016-09-29 18:04:45 -0700

    Add front-end verification to location review form

---
+++ public/javascripts/validation.js
@@ -0,0 +1,15 @@
+// listen for submit event of review form
+$('#addReview').submit(function (e) {
+  $('.alert.alert-danger').hide();
+  // check for any missing values
+  if (!$('input#name').val() || :$('select#rating').val() || !$('textarea#review').val()) {
+    // show or inject error message onto page if value is missing
+    if ($('.alert.alert-danger').length) {
+      $('alert.alert-danger').show();
+    } else {
+      $(this).prepend('<div role="alert" class="alert alert-danger">ALL fields required, please try again</div>');
+    }
+    // prevent form from submitting if value is missing
+    return false;
+  }
+});
