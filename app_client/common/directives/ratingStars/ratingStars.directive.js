25bc29261c91ba91389e59d5226e4e1a010c1671
Matthew Moreno <mmore500@gmail.com>
2016-10-02 12:26:41 -0700

    Implement ratingStars directive

---
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -0,0 +1,15 @@
+angular
+  .module('loc8rApp')
+  .directive('ratingStars', ratingStars);
+
+function ratingStars() {
+  return {
+    // only use the rating-stars directive when rating-stars is its own element
+    // or an attribute of another element
+    restrict : 'EA',
+    scope : {
+      thisRating : '=rating'
+    },
+    templateUrl: '/common/directive/ratingStars/ratingStars.template.html'
+  };
+}
