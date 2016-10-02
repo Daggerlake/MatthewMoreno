d365ec7bd215202774623ed7ec0a2bdd0090b219
Matthew Moreno <mmore500@gmail.com>
2016-10-01 23:52:21 -0700

    Add Angular filter to prettify distances

--- public/angular/loc8rApp.js
+++ public/angular/loc8rApp.js
@@ -20,6 +20,34 @@ var locationListCtrl = function ($scope) {
   }]};
 };
 
+var _isNumeric = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+// to be used as Angular filter, formatDistance function must return a function
+// that accepts distance parameter rather than accepting it itself
+var formatDistance = function () {
+  return function (distance) {
+    var numDistance, unit;
+    if (distance && _isNumeric(distance)) {
+      // if greater than 1,000m, display as km with one decimal unit
+      if (distance >= 1000) {
+        numDistance = parseFloat(distance / 1000).toFixed(1);
+        unit = 'km';
+      // otherwise, display as meters with no decimal units
+      } else {
+        numDistance = parseInt(distance);
+        unit = 'm';
+      }
+      return numDistance + unit;
+    } else {
+      return "?";
+    }
+  };
+};
+
+
 angular
   .module('loc8rApp')
-  .controller('locationListCtrl', locationListCtrl);
+  .controller('locationListCtrl', locationListCtrl)
+  .filter('formatDistance', formatDistance);
