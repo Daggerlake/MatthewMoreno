324b0428ffb8b2307d7a2a378d0adf715e1e5b84
Matthew Moreno <mmore500@gmail.com>
2016-10-02 10:57:03 -0700

    Create geolocation service

---
+++ app_client/common/services/geolocation.service.js
@@ -0,0 +1,21 @@
+// create geolocation service
+var geolocation = function () {
+  // Define function called getPosition that accepts three callback functions
+  // for success, error, and not supported
+  var getPosition = function (cbSuccess, cbError, cbNoGeo) {
+    // if geolocation supported...
+    if (navigator.geolocation) {
+      // ... call native method, passing through success and error callbacks
+      navigator.geolocation.getCurrentPosition(cbSuccess, cbError);
+    }
+    // if geolocation isn't supported...
+    else {
+      // ... invoke not supported callback
+      cbNoGeo();
+    }
+  };
+  return {
+    // return getPosition function so it can be invoked from controller
+    getPosition : getPosition
+  };
+};
