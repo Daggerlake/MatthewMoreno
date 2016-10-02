43cfcd073033898d03bb746e84ba6223ebb70a5d
Matthew Moreno <mmore500@gmail.com>
2016-10-02 10:10:20 -0700

    Add Angular-route to application

--- app_client/app.js
+++ app_client/app.js
@@ -1,1 +1,15 @@
-angular.module('loc8rApp', []);
+//Add ngRoute as a dependency
+angular.module('loc8rApp', ['ngRoute']);
+
+// Model config function to hold route definitions
+function config ($routeProvider) {
+  $routeProvider
+    .when('/', {
+    })
+    .otherwise({redirectTo: '/'});
+}
+
+// Add config to module, passing through $routeProvider as a dependency
+angular
+  .module('loc8rApp')
+  .config(['$routeProvider', config]);
