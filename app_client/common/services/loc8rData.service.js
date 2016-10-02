727eff23f347a582c947dc035a8a07a9e0618f51
Matthew Moreno <mmore500@gmail.com>
2016-10-02 10:56:33 -0700

    Create loc8rData service

---
+++ app_client/common/services/loc8rData.service.js
@@ -0,0 +1,11 @@
+// pass $http service into existing service function
+var loc8rData = function ($http) {
+  var locationByCoords = function(lat, lng) {
+    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&dmax=10000');
+  };
+  return {
+    // return locationByCoords function
+    // making it accessible as method of service
+    locationByCoords : locationByCoords
+  };
+};
