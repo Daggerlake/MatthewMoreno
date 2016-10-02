2ba41d9d6cd825f59577440417e9e139e239b933
Matthew Moreno <mmore500@gmail.com>
2016-10-02 10:27:34 -0700

    Implement Angular view for home list

--- app_client/app.js
+++ app_client/app.js
@@ -5,6 +5,7 @@ angular.module('loc8rApp', ['ngRoute']);
 function config ($routeProvider) {
   $routeProvider
     .when('/', {
+      templateUrl: 'home/home.view.html'
     })
     .otherwise({redirectTo: '/'});
 }
