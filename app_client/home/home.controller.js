eeb5fa9358f5178ae821e393cfb5c8adf7aa7758
Matthew Moreno <mmore500@gmail.com>
2016-10-02 10:53:41 -0700

    Refactor homepage to use controllerAs syntax

--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -5,12 +5,13 @@ angular
 
 // Define new controller homeCtrl
 // and bind some data for page header and sidebar
-function homeCtrl ($scope) {
-  $scope.pageHeader = {
+function homeCtrl () {
+  var vm = this;
+  vm.pageHeader = {
     title: 'Loc8r',
     strapline: 'Find places to work with wifi near you!'
   };
-  $scope.sidebar = {
+  vm.sidebar = {
     content: "Looking for wifi and a seat etc etc"
   };
 }
